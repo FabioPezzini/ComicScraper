30b634d17e261ffe6094a464cf253927439902df
Fabio Pezzini <pezzinif@gmail.com>
2019-08-06 17:13:04 +0200

    First

---
+++ comicscraper/comicscraper/items.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+
+import re
+
+
+def remove_unicode(value):
+    return (value.encode('ascii', 'ignore')).decode("utf-8")
+
+
+def remove_spaces(value):
+    return value.replace("\r\n", "")
+
+
+def format(value):
+    return re.sub('\s+', ' ', value).strip()
+
+
+def remove_various_author(value):
+    return value.replace("AA.VV.", "")
+
+
+class ComicscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field(
+        input_processor=MapCompose(remove_unicode, remove_spaces, format), output_processor=TakeFirst()
+    )
+    link = scrapy.Field()
+    price = scrapy.Field(input_processor=MapCompose(remove_unicode, remove_spaces, format), output_processor=TakeFirst())
+    publication_date = scrapy.Field()
+    series = scrapy.Field(input_processor=MapCompose(remove_unicode, remove_spaces, format), output_processor=TakeFirst())
+    subtitle = scrapy.Field(input_processor=MapCompose(remove_unicode, remove_spaces, format), output_processor=TakeFirst())
+    authors = scrapy.Field(input_processor=MapCompose(remove_unicode, remove_various_author, remove_spaces, format), output_processor=TakeFirst())
+    include = scrapy.Field(input_processor=MapCompose(remove_unicode, remove_spaces, format), output_processor=TakeFirst())
---
+++ comicscraper/comicscraper/spiders/panini_spider.py
@@ -0,0 +1,35 @@
+import scrapy
+
+from ..items import ComicscraperItem
+from scrapy.loader import ItemLoader
+
+
+class PaniniSpider(scrapy.Spider):
+    name = "panini"
+    start_urls = ["http://comics.panini.it/store/pub_ita_it/magazines.html"]
+
+    def parse(self, response):
+        # Get all the <a> tags
+        for sel in response.xpath("//div[@class='list-group']//h3/a"):
+            l = ItemLoader(item=ComicscraperItem(), selector=sel)
+            l.add_xpath('title', './text()')
+            l.add_xpath('link', './@href')
+            request = scrapy.Request(sel.xpath('./@href').get(), callback=self.parse_2page,
+                                     dont_filter=True)
+            request.meta['l'] = l
+            yield request
+
+            #next_page = response.xpath("//a[@class='next i-next']/@href").extract_first()
+            #if next_page is not None:
+                #next_page_link = response.urljoin(next_page)
+                #yield scrapy.Request(url=next_page_link)
+
+    def parse_2page(self, response):
+        l = response.meta['l']
+        l.add_value('price', response.xpath("//p[@class='special-price']//span/text()").get())
+        l.add_value('publication_date', response.xpath("//div[@id='data-pubblicazione']//h3/text()").get())
+        l.add_value('series', response.xpath("//h3[@class='serie']/text()").get())
+        l.add_value('subtitle', response.xpath("//small[@class='subtitle']/text()").get())
+        l.add_value('authors', response.xpath("//div[@id='authors']//div/text()").get())
+        l.add_value('include', response.xpath("//div[@id='includes']//div/text()").get())
+        return l.load_item()
